f782cbefc6e17507d82503d796e03bfc68ed2989
gpapadog <gpapadogeorgou@fas.harvard.edu>
2017-04-07 16:17:35 -0400

    add plotting

--- ImputeHeatInput_function.R
+++ ImputeHeatInput_function.R
@@ -1,13 +1,16 @@
 #' @param mean_na_cutoff A unit needs to have up to mean_na_cutoff percentage of
 #' missingness in order to impute its values.
 ImputeHeatInput <- function(dat_unit, year, month, method = c('kalman', 'seadec'),
-                            mean_na_cutoff = 0.5) {
+                            mean_na_cutoff = 0.5, plot_imputation = FALSE) {
   
   method <- match.arg(method)
   impute_function <- na.kalman
   if (method == 'seadec') {
     impute_function <- na.seadec
   }
+  
+  missing_start_which <- which(is.na(dat_unit$Heat.Input..MMBtu.))
+  missing_start <- length(missing_start_which)
   
   # Finding the unit ids with missing heat input.
   unique_uID <- with(dat_unit, unique(uID))
@@ -24,9 +27,9 @@ ImputeHeatInput <- function(dat_unit, year, month, method = c('kalman', 'seadec'
   missing_info[, 2] <- missing_info[, 2] + length(month) - missing_info[, 1]
   
   print('Number of observations per unit.')
-  table(missing_info[, 1])
+  print(table(missing_info[, 1]))
   print('Number of missing observations')
-  table(missing_info[, 2])
+  print(table(missing_info[, 2]))
   
   uIDs_with_missing <- rownames(missing_info)[missing_info[, 2] > 0]
   
@@ -67,12 +70,27 @@ ImputeHeatInput <- function(dat_unit, year, month, method = c('kalman', 'seadec'
     if (impute) {
       uID_imputed <- c(uID_imputed, uIDs_with_missing[ii])
       imp <- impute_function(D$Heat.Input..MMBtu.)
-      plotNA.distribution(D$Heat.Input..MMBtu.)
-      plotNA.imputations(D$Heat.Input..MMBtu., imp)
-      abline(v = wh_points, col = 'purple')
+      if (plot_imputation) {
+        plotNA.distribution(D$Heat.Input..MMBtu.)
+        plotNA.imputations(D$Heat.Input..MMBtu., imp)
+        abline(v = wh_points, col = 'purple')
+      }
+      dat_unit[dat_unit$uID == uIDs_with_missing[ii]]$Heat.Input..MMBtu. <- imp
     }
   }
   
+  missing_end_which <- which(is.na(dat_unit$Heat.Input..MMBtu.))
+  missing_end <- length(missing_end_which)
+  
+  predicted <- setdiff(missing_start_which, missing_end_which)
+  
+  print(paste('Initial number of missing entries:', missing_start))
+  print(paste('Number of imputed entries:', missing_start - missing_end))
+  print(paste('Number of imputed less than 0, that are set to 0:',
+              sum(dat_unit$Heat.Input..MMBtu.[predicted] < 0)))
+  dat_unit$Heat.Input..MMBtu.[predicted] <- 0
+  
+  return(dat_unit)
 }
 
 
